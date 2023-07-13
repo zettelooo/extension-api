918aa013ae886b1cc0463267ffd27cedc5d27f6e
Hessamoddin A Shokravi <ahs502@gmail.com>
2023-07-13 09:17:08 +0330

    Fix services type definition

--- src/ZettelExtensions/Services.ts
+++ src/ZettelExtensions/Services.ts
@@ -3,25 +3,31 @@ import { Id } from '@zettelooo/commons'
 // TODO: This needs strong typings shared with the life spans:
 
 export interface Services {
-  readonly provides?: readonly Services.Service[]
   readonly consumes?: readonly Services.Service[]
+  readonly provides?: readonly Services.Service[]
 }
 
 export namespace Services {
   export interface Service {
     readonly name: string
     readonly displayName?: string
-    readonly description: string
+    readonly description?: string
     readonly requestData?: {
       readonly description?: string
       readonly converters?: {
-        readonly [extensionId: Id]: (data: any) => any
+        readonly [extensionId: Id]: {
+          readonly serviceName: string
+          readonly converter: (data: any) => any
+        }
       }
     }
     readonly responseData?: {
       readonly description?: string
       readonly converters?: {
-        readonly [extensionId: Id]: (data: any) => any
+        readonly [extensionId: Id]: {
+          readonly serviceName: string
+          readonly converter: (data: any) => any
+        }
       }
     }
   }
